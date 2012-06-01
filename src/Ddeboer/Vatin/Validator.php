794f58ab8d015798509c07a2e63fc9655105090c
David de Boer <david@ddeboer.nl>
2012-06-01 11:48:33 +0200

    Add all patterns

--- src/Ddeboer/Vatin/Validator.php
+++ src/Ddeboer/Vatin/Validator.php
@@ -17,14 +17,34 @@ class Validator
      * Regular expression patterns per country code
      *
      * @var array
+     * @link http://ec.europa.eu/taxation_customs/vies/faq.html?locale=lt#item_11
      */
     protected $patterns = array(
         'AT' => 'U[A-Z\d]{8}',
         'BE' => '0\d{9}',
         'BG' => '\d{9,10}',
+        'CY' => '\d{8}[A-Z]',
+        'CZ' => '\d{8,10}',
         'DE' => '\d{9}',
-        'NL' => '\d{9}[A-Z]\d{2}',
-        'GB' => '\d{9}|\d{12}|(GD|HA)\d{3}'
+        'DK' => '(\d{2} ?){3}\d{2}',
+        'EE' => '\d{9}',
+        'EL' => '\d{9}',
+        'ES' => '[A-Z]\d{7}[A-Z]|\d{8}[A-Z]|[A-Z]\d{8}',
+        'FI' => '\d{8}',
+        'FR' => '([A-Z]{2}|\d{2})\d{9}',
+        'GB' => '\d{9}|\d{12}|(GD|HA)\d{3}',
+        'HU' => '\d{8}',
+        'IT' => '\d{11}',
+        'LT' => '(\d{9}|\d{12})',
+        'LV' => '\d{11}',
+        'MT' => '\d{8}',
+        'NL' => '\d{9}B\d{2}',
+        'PL' => '\d{10}',
+        'PT' => '\d{9}',
+        'RO' => '\d{2,10}',
+        'SE' => '\d{12}',
+        'SI' => '\d{8}',
+        'SK' => '\d{10}'
     );
 
     /**
