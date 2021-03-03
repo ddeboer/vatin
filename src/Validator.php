5f80a41627b848950b102dc1d5a84a39d81122f9
» lugges « <lukaskron@gmx.de>
2021-03-03 15:10:49 +0100

    Added non-EU VAT-IDs
    In favour of #3 (Non EU vat numbers)

--- src/Validator.php
+++ src/Validator.php
@@ -21,6 +21,7 @@ class Validator
      * @link http://ec.europa.eu/taxation_customs/vies/faq.html?locale=lt#item_11
      */
     private $patterns = array(
+    	# VAT EU
         'AT' => 'U[A-Z\d]{8}',
         'BE' => '[0|1]{1}\d{9}',
         'BG' => '\d{9,10}',
@@ -50,7 +51,35 @@ class Validator
         'RO' => '\d{2,10}',
         'SE' => '\d{12}',
         'SI' => '\d{8}',
-        'SK' => '\d{10}'
+        'SK' => '\d{10}',
+
+        # VAT NON-EU
+        'AL' => '[JKL]\d{8}[A-Z]',
+        'MK' => '\d{13}',
+        'AU' => '\d{11}',
+        'УНП' => '\d{9}',
+        'CA' => '.{9}',
+        'IS' => '.{5,6}',
+        'IN' => '\d{11}[VC]',
+        'ID' => '\d{15}',
+        'IL' => '\d{9}',
+        'KZ' => '\d{12}',
+        'MK' => '\d{13}',
+        'MC' => '([A-Z0-9]{2})\d{9}', # Monaco no CC in Wikipedia.
+        'NZ' => '\d{9}',
+        'NG' => '\d{8}[-]{1}\d{4}',
+        'PH' => '\d{12}',
+        'RU' => '\d{10}',
+        'SM' => '\d{5}',
+        'SA' => '\d{15}',
+        'RS' => '\d{9}',
+        'TR' => '\d{10}',
+        'UA' => '\d{12}',
+
+        # VAT Latin America
+        'AR' => '\d{11}',
+        'BO' => '\d{7}',
+
     );
 
     /**
